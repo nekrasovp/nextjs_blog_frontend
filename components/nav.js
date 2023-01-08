ae3061caf66b54324869d18ec979b99c27c2c187
Pavel Nekrasov <nekrasovp@gmail.com>
2023-01-08 10:43:42 +0300

    Init commit

---
+++ components/nav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+
+const Nav = ({ categories }) => {
+  return (
+    <div>
+      <nav className="uk-navbar-container">
+        <div className="uk-navbar-left">
+          <ul className="uk-navbar-nav">
+            <li>
+              <Link href="/">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="uk-navbar-right">
+          <ul className="uk-navbar-nav">
+            {categories.map((category) => {
+              return (
+                <li key={category.id}>
+                  <Link href={`/category/${category.attributes.slug}`} className="uk-link-reset">
+                    {category.attributes.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
